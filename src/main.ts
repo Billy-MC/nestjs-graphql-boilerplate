089fe3133beb8229e13f254047999d2e6ede55c7
Billy <billychuim@gmail.com>
2022-07-30 23:32:13 +1000

    add blank line

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.enableCors();
+
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalInterceptors(new TransformInterceptor());
 	app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }));
