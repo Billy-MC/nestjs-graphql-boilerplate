c1caea81dc46b72af2438322b4034b1e863692a1
Billy <billychuim@gmail.com>
2022-07-30 23:27:15 +1000

    eslint

--- src/hello-world/hello-world.resolver.spec.ts
+++ src/hello-world/hello-world.resolver.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HelloWorldResolver } from './hello-world.resolver';
 
 describe('HelloWorldResolver', () => {
-  let resolver: HelloWorldResolver;
+	let resolver: HelloWorldResolver;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [HelloWorldResolver],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [HelloWorldResolver],
+		}).compile();
 
-    resolver = module.get<HelloWorldResolver>(HelloWorldResolver);
-  });
+		resolver = module.get<HelloWorldResolver>(HelloWorldResolver);
+	});
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(resolver).toBeDefined();
+	});
 });
